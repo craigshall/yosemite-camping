3e85b9288340d7a1d9180bd173493751183e3b64
Craig Hall <craigsh@gmail.com>
2018-02-12 17:41:07 -0800

    Added header.txt file and cating it to the front of the email

--- campsites.sh
+++ campsites.sh
@@ -4,7 +4,7 @@ RESULTS=`grep -E '^.*[^\n]+.*$' results/result_$1_$2.txt | wc -l`
 if [ $RESULTS -gt "0" ];
 then
     echo "RESULTS FOUND - "`date`
-    echo results/result_$1_$2.txt | mail -s "Get this Campsite!" craigsh@gmail.com
+    cat results/header.txt results/result_$1_$2.txt | mail -s "Get this Campsite!" craigsh@gmail.com
 else
     echo "NO RESULTS FOUND - "`date`;
 fi;
